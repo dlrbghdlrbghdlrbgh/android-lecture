c7fa81301ffb993e98af9972a80407e874e5e6aa
이규호 <dlrbgh@ideadesignlab.kr>
2023-07-08 09:59:38 +0900

    lec4-1

---
+++ src/main/kotlin/lecture4/lec1.kt
@@ -0,0 +1,15 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+ 
+
+fun main() {	
+    
+	val intArray = arrayOf(1,2,3)
+    
+    val stringArray = arrayOf("이", "규", "호")
+    
+    val booleanArray = arrayOf(true, false, true)
+
+}

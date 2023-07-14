1f3d5d4e7fb3b5a4f861d409bb7b4b8e164f8fa3
이규호 <dlrbgh@ideadesignlab.kr>
2023-07-15 08:19:22 +0900

    조건문 문제 업로드

---
+++ src/main/kotlin/lecture5/lecture-2.kt
@@ -0,0 +1,11 @@
+//fun main(){
+//
+//
+//    val maxWindSeed = 30
+//
+//    /**
+//     * if ~ else if ~ else 를 사용하여 출력하기
+//     */
+//
+//
+//}
---
+++ src/main/kotlin/lecture5/lecture-4.kt
@@ -0,0 +1,17 @@
+//fun main(){
+//
+//    val lottoArray = arrayOf(0, 0, 0, 0, 0, 0)
+//
+//    /**
+//     * 아래는 반복문 중 하나인 while 문입니다.
+//     * while은 condition의 값이 true일때 {} 사이의 내용을 반복적으로 수행합니다
+//     * lottoArray의 값이 1~45의 수로 모두 채워졌을 때 while의 반복이 종료되도록
+//     * condition부분을 채워주세요
+//     */
+//
+////    while( condition ){
+////        val arrayIndex = (0..5).random()
+////        lottoArray[arrayIndex] = (1..45).random()
+////    }
+//
+//}

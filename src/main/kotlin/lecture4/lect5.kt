4e9cca729c21dd71985e9c224f6dff3bfb1020ac
이규호 <dlrbgh@ideadesignlab.kr>
2023-07-15 13:13:16 +0900

    배열 문제 주석처리

--- src/main/kotlin/lecture4/lect5.kt
+++ src/main/kotlin/lecture4/lect5.kt
@@ -1,25 +1,25 @@
-/**
- * 실습(3): 숫자 배열의 값을 10씩 늘리기
- */
-fun main() {
-
-    val intArray = arrayOf(1,2,3,4)
-
-    var index = 0
-
-    println(intArray[index]++)
-    println(intArray[index]++)
-    println(intArray[index]++)
-    println(intArray[index]++)
-
-    /**
-     * 코드를 완성해서 intArray값을 10 씩 늘려주세요
-     */
-
-
-    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
-    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
-    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
-    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
-
-}
+///**
+// * 실습(3): 숫자 배열의 값을 10씩 늘리기
+// */
+//fun main() {
+//
+//    val intArray = arrayOf(1,2,3,4)
+//
+//    var index = 0
+//
+//    println(intArray[index]++)
+//    println(intArray[index]++)
+//    println(intArray[index]++)
+//    println(intArray[index]++)
+//
+//    /**
+//     * 코드를 완성해서 intArray값을 10 씩 늘려주세요
+//     */
+//
+//
+//    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
+//    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
+//    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
+//    println(intArray[index]++)  // 원래의 값에서 10씩 늘어난 코드가 출력됩니다
+//
+//}

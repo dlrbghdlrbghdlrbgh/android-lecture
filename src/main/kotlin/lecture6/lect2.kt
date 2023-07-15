38d6d62e4865fbe0398cdf3a2bd13ed8ca72f45a
이규호 <dlrbgh@ideadesignlab.kr>
2023-07-15 13:21:45 +0900

    반복문 문제 업로드

---
+++ src/main/kotlin/lecture6/lect2.kt
@@ -0,0 +1,12 @@
+//package lecture6
+//
+//fun main(){
+//    var sum = 0
+//
+//    /**
+//     * 1부터 100까지의 합을 출력하는 반복문을 작성하세요
+//     */
+//
+//
+//    println("sum: "+ sum)
+//}
---
+++ src/main/kotlin/lecture6/lect5.kt
@@ -0,0 +1,29 @@
+///**
+// * You can edit, run, and share this code.
+// * play.kotlinlang.org
+// */
+//
+//
+//fun main() {
+//
+//    // 배열의 크기가 1000 이고 모든 배열의 값을 0으로 초기화하는 코드
+//    val intArray = IntArray(1000) { 0 }
+//
+//    /**
+//     * 1.반복문을 사용해서 intArray를 1~9까지의 임의의 값으로 입력해주세요
+//     */
+//
+//
+//    val pickArray = IntArray(10){0}
+//    /**
+//     * 2. 표본채취
+//     * - intArray의 값 중 값이 1인경우 pickArray에 순서대로 집어넣습니다
+//     * - intArray의 값 중 값이 5인경우 그 값을 0으로 초기화시킵니다
+//     * - intArray의 값이 9일때 아무것도 실행하지 않고 다음 순서를 진행합니다 (continue)
+//     * - pickArray의 10개 모두 1이 되었을 때 반복을 종료합니다(break)
+//     */
+//
+//    for(i in 0 until 10) {
+//
+//    }
+//}

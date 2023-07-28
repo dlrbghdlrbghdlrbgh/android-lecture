a8b572dd0d1b98175693221ac286aa383b26feff
이규호 <dlrbgh@ideadesignlab.kr>
2023-07-28 21:58:00 +0900

    lect 7 답 업데이트

--- src/main/kotlin/lecture6/answer/main.kt
+++ src/main/kotlin/lecture6/answer/main.kt
@@ -1,24 +1,44 @@
-package main.kotlin.lecture6.answer
-
-fun main(){
-    val hong = Customer()
-    val kim = Customer()
-
-    hong.name = "홍길동"
-    hong.email = "hong@email.com"
-    hong.phoneNumber = "010-0000-0000"
-    hong.allowMarketing = true
-
-    val hongsAccount = arrayListOf<Withdrawal>()
-    val hongsFirstAccount = Withdrawal()
-    hongsFirstAccount.accountNumber = "000-000-000"
-    hongsFirstAccount.isOpen = true
-    hongsFirstAccount.owner = "홍길동"
-    hongsFirstAccount.openDate = "2023-07-22"
-    hongsAccount.add(hongsFirstAccount)
-
-    hong.accounts = hongsAccount;
-
-
-
-}
+//package main.kotlin.lecture6.answer
+//
+//
+//fun main(){
+//    val kim = Customer()
+//
+//    kim.name = "이규호"
+//    kim.phoneNumber = "010-2190-1578"
+//    kim.email = "dlrbgh@ideadesignlab.kr"
+//    kim.allowMarketing = false
+//
+//    val kimsAccount = arrayListOf<Withdrawal>()
+//    val kimsFirstWithdrawl = Withdrawal()   // A
+//    var kimsSecondWithdrawal = Withdrawal() // B
+//
+//    kimsFirstWithdrawl.accountNumber = "3333-01-0001"
+//    kimsFirstWithdrawl.isOpen = true
+//    kimsFirstWithdrawl.openDate = "2023-07-10"
+//
+//    kimsAccount.add(kimsFirstWithdrawl)
+//
+//    kimsSecondWithdrawal.accountNumber = "3333-01-0002"
+//    kimsSecondWithdrawal.isOpen= false
+//    kimsSecondWithdrawal.openDate = "2020-07-10"
+//
+//    val incomeAccountHistory = AccountHistory()
+//    incomeAccountHistory.isIncome = true
+//    incomeAccountHistory.name = "김철수"
+//    incomeAccountHistory.date = "2020-07-15"
+//    incomeAccountHistory.howmuch = 50000000
+//
+//    kimsSecondWithdrawal.history.add(incomeAccountHistory)
+//
+//    val outcomeAccountHistory = AccountHistory()
+//    outcomeAccountHistory.isIncome = false
+//    outcomeAccountHistory.name = "김철수"
+//    outcomeAccountHistory.date = "2023-07-10"
+//    outcomeAccountHistory.howmuch = 50000000
+//
+//    kimsSecondWithdrawal.history.add(outcomeAccountHistory)
+//    kimsAccount.add(kimsSecondWithdrawal)
+//
+//    kim.accounts = kimsAccount
+//}
